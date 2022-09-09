7f559efb2cd9ed0cad106ba15bf4f4ae87db8d86
rosh021 <roshanbhattarai021@gmail.com>
2022-09-10 02:21:57 +1000

    working on sidebar

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Sidebar } from "../components/sidebar/Sidebar";
+
+export const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
