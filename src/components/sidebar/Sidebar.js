0c18f640e3c1e8b4f2a1f48acc4365c0cbf4f844
rosh021 <roshanbhattarai021@gmail.com>
2022-09-10 16:27:56 +1000

    working on UI design

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,38 @@
 import React from "react";
+import StarIcon from "@mui/icons-material/Star";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import VoicemailOutlinedIcon from "@mui/icons-material/VoicemailOutlined";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import DialpadTwoToneIcon from "@mui/icons-material/DialpadTwoTone";
+
+import "./Sidebar.css";
 
 export const Sidebar = () => {
-  return <div className="sidebar">fjdsfkl</div>;
+  return (
+    <div className="sidebar">
+      <div className="contactIcons">
+        <div className="icons">
+          <StarIcon />
+          <h6>Favorite</h6>
+        </div>
+        <div className="icons">
+          <AccessTimeIcon />
+          <h6>Recent</h6>
+        </div>
+        <div className="icons">
+          <AccountCircleIcon />
+          <h6>Contacts</h6>
+        </div>
+        <div className="icons">
+          <DialpadTwoToneIcon />
+          <h6>Keypad</h6>
+        </div>
+        <div className="icons">
+          <VoicemailOutlinedIcon />
+          <h6>Voicemail</h6>
+        </div>
+      </div>
+      <div className="list">asdas</div>
+    </div>
+  );
 };
